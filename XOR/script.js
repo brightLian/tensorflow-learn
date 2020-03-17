14e754bdbe83df45b02e49fb6e817d8b21a4d7c5
brightLian <brightLian@126.com>
2020-03-17 20:03:29 +0800

    feat: 尝试XOR问题

--- XOR/script.js
+++ XOR/script.js
@@ -1,7 +1,49 @@
 import * as tfvis from '@tensorflow/tfjs-vis'
 import * as tf from '@tensorflow/tfjs'
+import {getData} from './data'
 
 window.onload = async () => {
+    const data = getData(400);
 
+    tfvis.render.scatterplot(
+        {name: 'XOR训练数据'},
+        {
+            values: [
+                data.filter(p => p.label === 1),
+                data.filter(p => p.label === 0)
+            ]
+        }
+    );
 
+    const model = tf.sequential();
+    model.add(tf.layers.dense({
+        units: 4,
+        inputShape: [2],
+        activation: 'relu'
+    }));
+
+    model.add(tf.layers.dense({
+        units: 1,
+        activation: 'sigmoid'
+    }))
+
+    model.compile({
+        loss: tf.losses.logLoss,
+        optimizer: tf.train.adam(0.1)
+    });
+
+    const inputs = tf.tensor(data.map(p => [p.x, p.y]));
+    const labels = tf.tensor(data.map(p => p.label));
+
+    await model.fit(inputs, labels, {
+        epochs: 10,
+        callbacks: tfvis.show.fitCallbacks(
+            {name: 'XOR训练',},
+            ['loss'])
+    });
+
+    window.predict = (form) => {
+        const pred = model.predict(tf.tensor([[Number(form.x.value), Number(form.y.value)]]));
+        alert(`预测结果 ${pred.dataSync()[0]}`)
+    }
 };
